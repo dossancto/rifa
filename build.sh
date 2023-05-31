e5387ab871ebbbe1113288aed86bd17769e2e5d9
lu-css <dossantossilvalucas963@gmail.com>
2023-05-31 18:42:18 -0300

    add migrations

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ set -o errexit
 mix deps.get --only prod
 MIX_ENV=prod mix compile
 
+MIX_ENV=prod mix ecto.create
+MIX_ENV=prod mix ecto.migrate
+
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
