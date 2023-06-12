4c8b5382b58f60a5486758cd1a549ea200be90fe
lu-css <dossantossilvalucas963@gmail.com>
2023-06-12 14:42:38 -0300

    Add instruction message

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,15 +22,15 @@ import "../css/app.css"
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", info => topbar.show())
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
@@ -47,9 +47,5 @@ let radio = document.querySelectorAll(".tag_radion");
 let label = document.querySelector(".cards__container-items");
 
 radio.addEventListener('click', () => {
-
-
-    label.style.color = "#fff"
-
-   
+  label.style.color = "#fff"
 })
